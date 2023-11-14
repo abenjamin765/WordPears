45441b56678e44fc33e1bf5c7f564538973bc64e
mjay <michael@merged.group>
2023-11-13 22:05:40 -0500

    added axios and began refactoring to use backend

--- Main4.js
+++ Main4.js
@@ -1,38 +1,64 @@
+import { fetchWords, submitAnswer } from "./api";
+
 let roundTimer = 10;
 let intervalId;
 let roundNumber = 1;
 let themes;
 let roundTheme;
+let roundThemeId;
 let activeWords = [];
 let selectionOne, selectionTwo;
+let selections = [];
+let numberOfSelectionsToMake;
 
 async function initializeGame() {
   // Load the themes from the JSON file
-  const response = await fetch("themes.json");
-  const data = await response.json();
-  themes = data.Themes;
+  // const response = await fetch("themes.json");
+  // const data = await response.json();
+  // themes = data.Themes;
 
   // Set up the initial round and start the timer
-  setupRound();
+  await setupRound(1);
   startTimer();
 }
-function setupRound() {
-  // Select the round theme
-  roundTheme = themes[Math.floor(Math.random() * themes.length)];
-  activeWords = roundTheme.Words.slice(0, 2);
+async function setupRound(roundNumber) {
+  // // Select the round theme
+  // roundTheme = themes[Math.floor(Math.random() * themes.length)];
+  // activeWords = roundTheme.Words.slice(0, 2);
 
-  // Select 4 additional themes and extract one word from each
-  let addedThemes = 0;
-  while (addedThemes < 4) {
-    const tempTheme = themes[Math.floor(Math.random() * themes.length)];
-    if (
-      tempTheme.Theme !== roundTheme.Theme &&
-      !activeWords.includes(tempTheme.Words[0])
-    ) {
-      activeWords.push(tempTheme.Words[0]);
-      addedThemes++;
-    }
+  // // Select 4 additional themes and extract one word from each
+  // let addedThemes = 0;
+  // while (addedThemes < 4) {
+  //   const tempTheme = themes[Math.floor(Math.random() * themes.length)];
+  //   if (
+  //     tempTheme.Theme !== roundTheme.Theme &&
+  //     !activeWords.includes(tempTheme.Words[0])
+  //   ) {
+  //     activeWords.push(tempTheme.Words[0]);
+  //     addedThemes++;
+  //   }
+  // }
+
+  const { theme, words } = await fetchWords(roundNumber);
+  roundThemeId = theme;
+  switch (roundNumber) {
+    case 2:
+    case 3:
+      numberOfSelectionsToMake = 2;
+      break;
+    case 1:
+    default:
+      numberOfSelectionsToMake = 2;
+      break;
+
   }
+
+  if (words.length <= numberOfSelectionsToMake) {
+    console.error('There should be more words to choose from than need to be selected')
+  }
+
+  // Capable of pushing N words instead of just N = 6
+  activeWords.push(...words);
 
   displayWords();
 }
@@ -46,37 +72,68 @@ function displayWords() {
     wordListElem.appendChild(btn);
   });
 }
+
+// Consider refactor which adds buttons to an array
+// Every time a button is added to the array, a check is made: is the length of the array gte the round's selected number
+// If so, call check solution
 function selectWord(event) {
-  const button = event.target;
-  if (!selectionOne) {
-    selectionOne = button;
-  } else if (!selectionTwo && selectionOne !== button) {
-    selectionTwo = button;
-  } else {
-    return;
-  }
+  // const button = event.target;
+  // if (!selectionOne) {
+  //   selectionOne = button;
+  // } else if (!selectionTwo && selectionOne !== button) {
+  //   selectionTwo = button;
+  // } else {
+  //   return;
+  // }
 
   button.classList.add("selected");
+  selections.push(button);
 
-  if (selectionOne && selectionTwo) {
+  // if (selectionOne && selectionTwo) {
+  //   validateSelection();
+  // }
+  if (selections.length > numberOfSelectionsToMake) {
+    console.error('There are more selected values than are supposed to be selected for this round');
+  }
+  if (selections.length === numberOfSelectionsToMake) {
     validateSelection();
   }
 }
 
-function validateSelection() {
-  const word1 = selectionOne.innerText;
-  const word2 = selectionTwo.innerText;
 
-  if (roundTheme.Words.includes(word1) && roundTheme.Words.includes(word2)) {
-    selectionOne.classList.add("correct");
-    selectionTwo.classList.add("correct");
-    document.getElementById("theme").innerText = roundTheme.Theme;
+function validateSelection() {
+  // const word1 = selectionOne.innerText;
+  // const word2 = selectionTwo.innerText;
+
+  // if (roundTheme.Words.includes(word1) && roundTheme.Words.includes(word2)) {
+  //   selectionOne.classList.add("correct");
+  //   selectionTwo.classList.add("correct");
+  //   document.getElementById("theme").innerText = roundTheme.Theme;
+  //   setTimeout(transitionToNextRound, 1000);
+  // } else {
+  //   selectionOne.classList.remove("selected");
+  //   selectionTwo.classList.remove("selected");
+  //   selectionOne = null;
+  //   selectionTwo = null;
+  // }
+  const wordsToValidate = selections.map(selection => selection.innerText);
+  const { isCorrect, theme } = await submitAnswer(roundThemeId, wordsToValidate);
+  // If isCorrect is true, theme should be included and is an error if not
+  if (isCorrect) {
+    if (!theme) {
+      console.error('Theme missing from api response')
+    }
+
+    for (const selection of selections) {
+      selection.classList.add("correct");
+    }
+    document.getElementById("theme").innerText = theme;
     setTimeout(transitionToNextRound, 1000);
   } else {
-    selectionOne.classList.remove("selected");
-    selectionTwo.classList.remove("selected");
-    selectionOne = null;
-    selectionTwo = null;
+    for (const selection of selections) {
+      selection.classList.remove("selected");
+      selections = [];
+    }
   }
 }
 function transitionToNextRound() {
@@ -95,7 +152,7 @@ function transitionToNextRound() {
     .filter((word) => !activeWords.includes(word));
   const randomWordFromOtherThemes =
     wordsFromOtherThemes[
-      Math.floor(Math.random() * wordsFromOtherThemes.length)
+    Math.floor(Math.random() * wordsFromOtherThemes.length)
     ];
 
   // Choose a word from the new theme
@@ -126,6 +183,7 @@ function startTimer() {
     document.getElementById("time").value = roundTimer;
     if (roundTimer === 0) {
       clearInterval(intervalId);
+      // Need to get the theme name from the theme id
       alert(`Game Over. The theme was ${roundTheme.Theme}.`);
     }
   }, 1000);
---
+++ api.js
@@ -0,0 +1,41 @@
+import axios from 'axios'
+
+const API_BASE_URL = 'http://localhost:8000'
+
+/**
+ * 
+ * @param {int} round  
+ */
+export async function fetchWords(roundNumber) {
+    try {
+        const response = await axios.get(`${API_BASE_URL}/get-words`, { params: { round: roundNumber } });
+        return response.data;
+    } catch (err) {
+        // We'll need better error handling
+        console.error('Error fetching game data: ', error);
+        return { theme: '', words: [] }
+    }
+}
+
+/**
+ * 
+ * @param {string} themeId 
+ * @param {string[]} words 
+ */
+export async function submitAnswer(themeId, words) {
+    try {
+        const response = await axios.post(`${API_BASE_URL}/check-match`, {
+            themeId,
+            words
+        });
+        return response.data;
+        // Shape of response.data
+        // {
+        //     isCorrect: boolean,
+        //     theme?: string // the theme name
+        // }
+    } catch (err) {
+        console.error('Error submitting answer:', err);
+        throw err;
+    }
+}
